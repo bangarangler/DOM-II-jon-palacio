7681af31aaf30ff4e263a60188aacf5ca30bdf34
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-01-29 15:19:04 -0500

    Added keydown event body and click event on heading
    anywhere in body press key to key alert of what key you pressed.
    
    also added css style to .logo-header to show that you can click.
    with js when you click a border-bottom is applied. have to add in
    event to remove.

--- js/index.js
+++ js/index.js
@@ -10,3 +10,35 @@ coolBtn[0].addEventListener("mouseover", e => {
 coolBtn[0].addEventListener("mouseout", e => {
   coolBtn[0].style.transform = "scale(1.0)";
 });
+
+//BTN SCALE FOR BUTTON IN MIDDLE
+coolBtn[1].addEventListener("mouseover", e => {
+  coolBtn[1].style.transform = "scale(1.1)";
+});
+
+coolBtn[1].addEventListener("mouseout", e => {
+  coolBtn[1].style.transform = "scale(1.0)";
+});
+
+//BTN SCALE FOR BUTTON ON RIGHT
+coolBtn[2].addEventListener("mouseover", e => {
+  coolBtn[2].style.transform = "scale(1.1)";
+});
+
+coolBtn[2].addEventListener("mouseout", e => {
+  coolBtn[2].style.transform = "scale(1.0)";
+});
+
+//KEYDOWN EVENT POPUP ALERT
+const alertPopup = document.querySelector("body");
+
+alertPopup.addEventListener("keydown", e => {
+  alert(`You pressed ${e.code}`);
+});
+
+//FUNBUS HEADING UNDERLINE ON CLICK
+const funBusHeading = document.querySelector(".logo-heading");
+
+funBusHeading.addEventListener("click", e => {
+  funBusHeading.style.borderBottom = "1px solid aqua";
+});
