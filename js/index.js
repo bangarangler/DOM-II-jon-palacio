b9a2c98fe14d211b4fcd149622d4f2e624df05ef
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-01-29 15:00:00 -0500

    Event listener for btn animation scale
    add listener for button on bottom left to scale. need to add it
    to other buttons as well.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const coolBtn = document.querySelectorAll(".btn");
+
+// MOUSEOVER BTN SCALSES THE BUTTON
+coolBtn[0].addEventListener("mouseover", e => {
+  coolBtn[0].style.transform = "scale(1.1)";
+});
+
+//MOUSEOUT BTN RETURNS TO NORMAL
+coolBtn[0].addEventListener("mouseout", e => {
+  coolBtn[0].style.transform = "scale(1.0)";
+});
